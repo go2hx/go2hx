1b42fa6b4089eae3a6a5eb618d6cc8e907ffd110
PXshadow <joeyprather8@gmail.com>
2020-11-04 03:31:28 -0800

    remove examples folder, instead optional to clone gobyexample in repo

--- run.sh
+++ run.sh
@@ -1,2 +1,3 @@
-go run packages.go github.com/pxshadow/go2hx/examples/constants/
+git clone https://github.com/mmcgrana/gobyexample
+go run packages.go github.com/pxshadow/go2hx/gobyexample/examples/constants/
 go run golang.org/x/tools/cmd/ssadump/ -build=F -test fmt

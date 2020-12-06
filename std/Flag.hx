6579bb4e1f4765e5f661a28b7225df7684e980f4
PXshadow <joeyprather8@gmail.com>
2020-12-05 20:03:39 -0800

    inital flag system, and remove build warnings

--- std/Flag.hx
+++ std/Flag.hx
@@ -6,22 +6,22 @@ class Flag {
     }
     public static function parse() {}
     public static function string(flag:String,value:String,desc:String):Pointer<String> {
-        return Pointer.make("");
+        return Pointer.make(() -> value,(x) -> value = x);
     }
     public static function int(flag:String,value:Int,desc:String):Pointer<Int> {
-        return Pointer.make(0);
+        return Pointer.make(() -> value,(x) -> value = x);
     }
     public static function bool(flag:String,value:Bool,desc:String):Pointer<Bool> {
-        return Pointer.make(false);
+        return Pointer.make(() -> value,(x) -> value);
     }
     //Pointer ref of flag
-    public static function stringVar(p:Any,flag:String,value:String,desc:String) {
-
+    public static function stringVar(p:Pointer<String>,flag:String,value:String,desc:String) {
+        p.value = value;
     }
-    public static function intVar(p:Any,flag:String,value:Int,desc:String) {
-
+    public static function intVar(p:Pointer<Int>,flag:String,value:Int,desc:String) {
+        p.value = value;
     }
-    public static function boolVar(p:Any,flag:String,value:Bool,desc:String) {
-
+    public static function boolVar(p:Pointer<Bool>,flag:String,value:Bool,desc:String) {
+        p.value = value;
     }
 }

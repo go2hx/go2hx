9839ac2356ff5cbf964dd5c74e2022fda92fb8fd
PXshadow <joeyprather8@gmail.com>
2021-04-03 23:12:50 -0700

    keyvalueiterator for GoArray

--- stdgo/GoArray.hx
+++ stdgo/GoArray.hx
@@ -19,6 +19,10 @@ abstract GoArray<T>(Vector<T>) from Vector<T> {
 		return new VectorIterator(this);
 	}
 
+	inline public function keyValueIterator():KeyValueIterator<GoInt, T> {
+		return new GoArrayKeyValueIterator(this);
+	}
+
 	public inline function new(args:Rest<T>) {
 		this = new Vector<T>(args.length);
 		for (i in 0...args.length) {
@@ -95,6 +99,24 @@ abstract GoArray<T>(Vector<T>) from Vector<T> {
 
 	inline public function toString() {
 		return this.toArray().toString();
+	}
+}
+
+
+private class GoArrayKeyValueIterator<T> {
+	var pos:Int = 0;
+	var vector:Vector<T>;
+
+	public inline function new(vector) {
+		this.vector = vector;
+	}
+
+	public inline function hasNext() {
+		return pos < vector.length;
+	}
+
+	public inline function next() {
+		return {key: (pos : GoInt), value: vector.get(pos++)};
 	}
 }
 

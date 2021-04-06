574a83e6e41aaa8bbbb13adf5e915776add0bd73
PXshadow <joeyprather8@gmail.com>
2021-04-05 22:36:24 -0700

    init wait group

--- stdgo/sync/Sync.hx
+++ stdgo/sync/Sync.hx
@@ -1,6 +1,8 @@
 package stdgo.sync;
 
-import stdgo.StdGoTypes.StructType;
+import sys.thread.Lock;
+import stdgo.StdGoTypes;
+import stdgo.StdGoTypes;
 import sys.thread.Mutex;
 
 class RWMutex implements StructType {
@@ -22,4 +24,36 @@ class RWMutex implements StructType {
 	public function rlock() {}
 
 	public function runlock() {}
+}
+
+
+class WaitGroup implements StructType {
+	public var _is_pointer_:Bool = false;
+	var lock:Lock;
+	public final _typeName_:String = "WaitGroup";
+	public var _address_:Int = 0;
+	var counter:GoUInt = 0;
+	public function new() {
+		_address_ = Go.addressIndex++;
+		lock = new Lock();
+	}
+	public function add(delta:GoInt) {
+		counter += delta;
+		if (counter < 0)
+			throw "sync: negative WaitGroup counter";
+		//TODO: check for overflow
+		if (delta < 0)
+			checkFinish();
+	}
+	public function done() {
+		counter--;
+		checkFinish();
+	}
+	public function wait() {
+		lock.wait();
+	}
+	function checkFinish() {
+		if (counter <= 0)
+			lock.release();
+	}
 }

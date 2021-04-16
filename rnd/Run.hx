9938c1521e27accd2122135f50794ca662588fee
PXshadow <joeyprather8@gmail.com>
2021-04-16 01:17:48 -0700

    Init work for statically typed method of Accessing Haxe from Go, using a macro generator. #24

--- rnd/Run.hx
+++ rnd/Run.hx
@@ -5,7 +5,7 @@ function main() {
 	Main.init(["-ident", "./rnd"]);
 	for (path in Main.exportPaths) {
 		path = StringTools.replace(path, "/", ".");
-		var command = 'haxe -cp golibs -main $path --interp';
+		var command = 'haxe -cp golibs -main $path --macro stdgo.internal.GoGen.build() --interp';
 		trace(command);
 		Sys.command(command);
 	}

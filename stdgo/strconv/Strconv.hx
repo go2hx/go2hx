393eb2ffa4c23d56d3436788c6c642288a4cb3cc
PXshadow <joeyprather8@gmail.com>
2021-06-05 21:47:35 -0700

    improve/update stdgo.Strconv

--- stdgo/strconv/Strconv.hx
+++ stdgo/strconv/Strconv.hx
@@ -1,9 +1,11 @@
 package stdgo.strconv;
 
+import stdgo.StdGoTypes.GoInt64;
+import stdgo.StdGoTypes.Error;
 import stdgo.internal.ErrorReturn;
 import stdgo.StdGoTypes.MultiReturn;
 
-function parseFloat(s:String, bitSize:Int):MultiReturn<ErrorReturn<Float>> {
+function parseFloat(s:String, bitSize:GoInt64):MultiReturn<ErrorReturn<Float>> {
 	try {
 		return {value: Std.parseFloat(s)};
 	} catch (e) {
@@ -11,7 +13,7 @@ function parseFloat(s:String, bitSize:Int):MultiReturn<ErrorReturn<Float>> {
 	}
 }
 
-inline function parseInt(s:String, base:Int, bitSize:Int):MultiReturn<ErrorReturn<Int>> {
+inline function parseInt(s:String, base:GoInt64, bitSize:GoInt64):MultiReturn<ErrorReturn<Int>> {
 	try {
 		var value = Std.parseInt(s);
 		if (value == null)
@@ -25,11 +27,32 @@ inline function parseInt(s:String, base:Int, bitSize:Int):MultiReturn<ErrorRetur
 }
 
 inline function parseBool(s:String):MultiReturn<ErrorReturn<Bool>> {
-	var bool = s == "true";
-	return {value: bool, error: null};
+	return switch s {
+		case "1","t","T","true","TRUE","True":
+			{value: true};
+		case "0", "f", "F", "false", "FALSE", "False":
+			{value: false};
+		default:
+			{value: false, error: syntaxError("parseBool",s)};
+	}
 }
 
-inline function parseUint(s:String, base:Int, bitSize:Int):MultiReturn<ErrorReturn<Int>> {
+inline function formatBool(b:Bool):GoString
+	return b ? "true" : "false";
+
+
+private final errRange = stdgo.errors.Errors.new_("value out of range");
+private final errSyntax = stdgo.errors.Errors.new_("invalid syntax");
+
+private function syntaxError(fn:String, str:String):NumError {
+	return new NumError(fn,str,errSyntax);
+}
+
+private function rangeError(fn:String,str:String):NumError {
+	return new NumError(fn,str,errRange);
+}
+
+inline function parseUint(s:String, base:GoInt64, bitSize:GoInt64):MultiReturn<ErrorReturn<Int>> {
 	return parseInt(s, base, bitSize);
 }
 
@@ -38,3 +61,20 @@ inline function parseUint(s:String, base:Int, bitSize:Int):MultiReturn<ErrorRetu
 inline function atoi(s:String) {
 	return parseInt(s, 0, 0);
 }
+
+class NumError implements Error {
+	public var func:GoString;
+	public var num:GoString;
+	public var err:Error;
+	public function new(func,num,err) {
+		this.func = func;
+		this.num = num;
+		this.err = err;
+	}
+	public function error():GoString
+		return this.err.error();
+	public function unwrap():Error
+		return this.err;
+}
+
+final intSize:GoInt64 = "9223372036854775807";

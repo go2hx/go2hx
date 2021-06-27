006e9b57da17481c5cbfb275397643cfcd67f797
PXshadow <joeyprather8@gmail.com>
2021-06-26 21:16:55 -0700

    add boiler plate Math methods

--- stdgo/math/Math.hx
+++ stdgo/math/Math.hx
@@ -1,76 +1,177 @@
 package stdgo.math;
 
-import stdgo.StdGoTypes.GoFloat64;
+import stdgo.StdGoTypes.MultiReturn;
+import stdgo.StdGoTypes.GoFloat64 as Float;
 import stdgo.StdGoTypes.GoInt;
 import Math as M;
 
 final pi = M.PI;
 
-inline function max(a, b) {
-	return M.max(a, b);
+inline function max(a:Float, b:Float):Float {
+	return M.max(a.toBasic(), b.toBasic());
 }
 
-inline function inf(sign:GoInt) {
+inline function min(a:Float, b:Float):Float {
+	return M.min(a.toBasic(), b.toBasic());
+}
+
+inline function inf(sign:GoInt):Float {
 	if (sign >= 0)
-		return Math.POSITIVE_INFINITY;
-	return Math.NEGATIVE_INFINITY;
+		return M.POSITIVE_INFINITY;
+	return M.NEGATIVE_INFINITY;
 }
 
-inline function signbit(x:GoFloat64) {
+inline function signbit(x:Float):Bool {
 	return x <= 0;
 }
 
-inline function isNaN(x:GoFloat64)
+inline function isNaN(x:Float)
 	return M.isNaN(x.toBasic());
 
-inline function min(a, b) {
-	return M.min(a, b);
+inline function abs(x:Float):Float {
+	return M.abs(x.toBasic());
 }
 
-inline function abs(v) {
-	return M.abs(v);
+inline function exp(x:Float):Float {
+	return M.exp(x.toBasic());
 }
 
-inline function exp(v:GoFloat64) {
-	return M.exp(v.toBasic());
+inline function acos(x:Float):Float {
+	return M.acos(x.toBasic());
 }
 
-inline function acos(v) {
-	return M.acos(v);
+inline function cos(x:Float):Float {
+	return M.cos(x.toBasic());
 }
 
-inline function acosh(v) {
-	return 0;
+inline function acosh(x:Float):Float {
+	return 0; //TODO
 }
 
-inline function asin(v) {
-	return M.asin(v);
+inline function atanh(x:Float):Float {
+	return 0; //TODO
 }
 
-inline function asinh(v) {
-	return 0;
+inline function cosh(x:Float):Float {
+	return 0; //TODO
 }
 
-inline function atan(v) {
-	return M.atan(v);
+inline function cbrt(x:Float):Float {
+	return 0; //TODO
 }
 
-inline function atan2(y, x) {
-	return M.atan2(y, x);
+inline function erf(x:Float):Float {
+	return 0; //TODO
 }
 
-inline function ceil(v) {
-	return M.ceil(v);
+inline function erfc(x:Float):Float {
+	return 0; //TODO
 }
 
-inline function naN() {
+inline function exp2(x:Float):Float {
+	return 0; //TODO
+}
+
+inline function expm1(x:Float):Float {
+	return 0; //TODO
+}
+
+inline function frexp(x:Float):MultiReturn<{frac:Float,exp:GoInt}> {
+	return {frac: 0, exp: 0}; //TODO
+}
+
+inline function hypot(x:Float,y:Float):Float {
+	return 0; //TODO
+}
+
+inline function mod(x:Float,y:Float):Float {
+	return 0; //TODO
+}
+
+inline function floor(x:Float):Float {
+	return M.ffloor(x.toBasic());
+}
+
+inline function pow(x:Float,y:Float):Float {
+	return M.pow(x.toBasic(),y.toBasic());
+}
+
+inline function trunc(x:Float):Float {
+	return 0; //TODO
+}
+
+inline function remainder(x:Float,y:Float):Float {
+	return 0; //TODO
+}
+
+inline function modf(x:Float):MultiReturn<{int:Float,frac:Float}> {
+	return {int: 0, frac: 0}; //TODO
+}
+
+inline function ldexp(x:Float,y:Float):Float {
+	return 0; //TODO
+}
+
+inline function log(x:Float):Float {
+	return M.log(x.toBasic());
+}
+inline function logp(x:Float):Float {
+	return 0; //TODO
+}
+
+inline function log1p(x:Float):Float {
+	return 0; //TODO
+}
+
+inline function log10(x:Float):Float {
+	return 0; //TODO
+}
+
+inline function log2(x:Float):Float {
+	return 0; //TODO
+}
+
+
+inline function asin(x:Float):Float {
+	return M.asin(x.toBasic());
+}
+
+inline function asinh(x:Float):Float {
+	return 0; //TODO
+}
+
+inline function sinh(x:Float):Float {
+	return 0; //TODO
+}
+
+inline function tanh(x:Float):Float {
+	return 0; //TODO
+}
+
+inline function tan(x:Float):Float {
+	return M.tan(x.toBasic());
+}
+
+inline function atan(x:Float):Float {
+	return M.atan(x.toBasic());
+}
+
+inline function atan2(y:Float, x:Float):Float {
+	return M.atan2(y.toBasic(), x.toBasic());
+}
+
+inline function ceil(x:Float):Float {
+	return M.ceil(x.toBasic());
+}
+
+inline function naN():Float {
 	return M.NaN;
 }
 
-inline function round(v) {
-	return M.round(v);
+inline function round(x:Float):Float {
+	return M.round(x.toBasic());
 }
 
-inline function sin(v) {
-	return M.sin(v);
+inline function sin(x:Float):Float {
+	return M.sin(x.toBasic());
 }

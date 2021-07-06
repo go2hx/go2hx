7629ba5e0b490d18476f4a5b98a3502feebdaad0
PXshadow <joeyprather8@gmail.com>
2021-07-05 19:45:00 -0700

    starting base for unsafe pointer

--- stdgo/unsafe/Unsafe.hx
+++ stdgo/unsafe/Unsafe.hx
@@ -3,24 +3,41 @@ package stdgo.unsafe;
 import stdgo.StdGoTypes.AnyInterface;
 import stdgo.StdGoTypes.GoUIntptr;
 import stdgo.Pointer as BasePointer;
+//Pointer -> uintptr
+//Pointer -> pointer
+class PointerData {
+	public var get:() -> Dynamic;
+	public var set:Dynamic->Dynamic;
+	public var address:Int;
 
-abstract Pointer(BasePointer<Dynamic>) to BasePointer<Dynamic> from BasePointer<Dynamic> {
-	public function new(obj) {
-		this = new Pointer(obj);
+	public function new(get, set, address) {
+		this.get = get;
+		this.set = set;
+		this.address = address;
+	}
+
+	public inline function toString() {
+		return "* " + Std.string(get());
+	}
+}
+abstract Pointer(PointerData) from PointerData {
+	public inline function new(obj:PointerData) {
+		this = obj;
+	}
+	@:to
+	static function toAddress(x:Pointer):GoUIntptr {
+		return 0;
 	}
 }
 
+function sizeof(x:AnyInterface):GoUIntptr {
+	return x.type.size();
+}
 
-typedef ArbitraryType = AnyInterface;
-
-function sizeof(x:ArbitraryType):GoUIntptr {
+function offsetof(x:AnyInterface):GoUIntptr {
 	return 0;
 }
 
-function offsetof(x:ArbitraryType):GoUIntptr {
-	return 0;
-}
-
-function alignof(x:ArbitraryType):GoUIntptr {
+function alignof(x:AnyInterface):GoUIntptr {
 	return 0;
 }

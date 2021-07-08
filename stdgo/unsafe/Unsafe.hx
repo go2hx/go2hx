6d6a196d3aa8cfd19876e199a56c4e4f1c4b8999
PXshadow <joeyprather8@gmail.com>
2021-07-08 01:01:17 -0700

    simplify unsafe pointer again

--- stdgo/unsafe/Unsafe.hx
+++ stdgo/unsafe/Unsafe.hx
@@ -4,33 +4,8 @@ import stdgo.StdGoTypes.AnyInterface;
 import stdgo.StdGoTypes.GoUIntptr;
 import stdgo.Pointer as BasePointer;
 
-// Pointer -> uintptr
-// Pointer -> pointer
-class PointerData {
-	public var get:() -> Dynamic;
-	public var set:Dynamic->Dynamic;
-	public var address:Int;
-
-	public function new(get, set, address) {
-		this.get = get;
-		this.set = set;
-		this.address = address;
-	}
-
-	public inline function toString() {
-		return "* " + Std.string(get());
-	}
-}
-
-abstract Pointer(PointerData) from PointerData {
-	public inline function new(obj:PointerData) {
-		this = obj;
-	}
-
-	@:to
-	static function toAddress(x:Pointer):GoUIntptr {
-		return 0;
-	}
+abstract Pointer(BasePointer<Dynamic>) from BasePointer<Dynamic> to BasePointer<Dynamic> {
+	
 }
 
 function sizeof(x:AnyInterface):GoUIntptr {

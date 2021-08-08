688da56ab97d213d6f4083b94be1ebeaeea9af02
PXshadow <joeyprather8@gmail.com>
2021-08-08 02:20:04 -0700

    add int for rand

--- stdgo/math/Rand.hx
+++ stdgo/math/Rand.hx
@@ -1,9 +1,15 @@
 package stdgo.math;
 
-import stdgo.StdGoTypes.GoInt32;
+import stdgo.StdGoTypes;
 
 function int31n(n:GoInt32):GoInt32
 	return Std.random(n.toBasic());
 
 function int31():GoInt32
+	return int31n(2147483647);
+
+function intn(n:GoInt):GoInt
+	return Std.random(n.toBasic());
+
+function int():GoInt
 	return int31n(2147483647);

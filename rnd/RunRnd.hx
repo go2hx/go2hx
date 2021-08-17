90aa7b37f44c07f594c101b70dc899ddaf266152
PXshadow <joeyprather8@gmail.com>
2021-08-17 01:14:40 -0700

    change config of runner

--- rnd/RunRnd.hx
+++ rnd/RunRnd.hx
@@ -7,16 +7,15 @@ function main() {
 		Main.onComplete = (modules, data) -> {
 			if (modules.length == 0)
 				throw "no exported path";
-			var path = Util.modulePath(modules[0]);
-			var command = 'haxe -cp golibs -main $path --interp';
+			final path = Util.modulePath(modules[0]);
+			final command = 'haxe -cp golibs -main $path --interp';
+			Sys.println(command);
 			Sys.command(command);
 			Sys.println("\n~~~~~~~~~~~~expected~~~~~~~~~~~~");
 			Sys.command("go run ./rnd");
 			Main.close();
 		};
-		trace("compile");
 		Sys.println("compile: " + Main.compile(["./rnd", Sys.getCwd()]));
-		trace("done");
 	});
 	while (true)
 		Main.update();

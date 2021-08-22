9a3e535b8898bb28f1686318b9588057565813b0
PXshadow <joeyprather8@gmail.com>
2021-08-21 23:47:51 -0700

    init macro for goto control flow

--- stdgo/internal/Macro.hx
+++ stdgo/internal/Macro.hx
@@ -25,6 +25,112 @@ class Macro {
 		return macro $b{exprs};
 	}
 
+	public static macro function controlFlow(body:Expr) {
+		var selectionName = "____select____";
+		var exitName = "____exit____";
+		var func = null;
+		var cases:Array<Case> = [];
+		var vars:Array<Var> = [];
+
+		function loop(e:Expr, inLoop:Bool):Expr {
+			return macro {
+				$e;
+				if ($i{exitName})
+					${inLoop?macro break:macro continue};
+			}
+		}
+		func = function(expr:haxe.macro.Expr, inLoop:Bool):Expr {
+			return switch (expr.expr) {
+				case EMeta(s, e):
+					switch s.name {
+						case ":goto":
+							macro {
+								$i{selectionName} = $e;
+								${inLoop ? macro {$i{exitName} = true; break;} : macro continue};
+							};
+						default:
+							expr;
+					}
+				case EWhile(econd, e, normalWhile):
+					expr.expr = EWhile(econd, func(e, true), normalWhile);
+					expr = loop(expr, inLoop);
+					expr;
+				case EFor(it, e):
+					expr.expr = EFor(it, func(e, true));
+					expr = loop(expr, inLoop);
+					expr;
+				case EBlock(exprs):
+					for (i in 0...exprs.length)
+						exprs[i] = func(exprs[i], inLoop);
+					expr.expr = EBlock(exprs);
+					expr;
+				case EIf(econd, eif, eelse):
+					expr.expr = EIf(econd, func(eif, inLoop), eelse == null ? null : func(eelse, inLoop));
+					expr;
+				case EVars(v):
+					vars = vars.concat(v);
+					macro {};
+				case ECall(e, params):
+					var printer = new haxe.macro.Printer();
+					final str = printer.printExpr(e);
+					trace(str);
+					if (str == "Go.cfor") {
+						var block = params.pop();
+						block = func(block, true);
+						params.push(block);
+						expr.expr = ECall(e, params);
+						expr = loop(expr, inLoop);
+						expr;
+					}
+					expr;
+				default:
+					expr;
+			}
+		}
+		body = func(body, false);
+		switch body.expr {
+			case EBlock(exprs):
+				for (i in 0...exprs.length) {
+					switch exprs[i].expr {
+						case EMeta(s, e):
+							switch s.name {
+								case ":label":
+									if (cases.length == 0) {
+										// first case
+										cases.push({values: [macro ""], expr: {expr: EBlock(exprs.slice(0, i)), pos: Context.currentPos()}});
+									}
+									cases.push({values: [s.params[0]], expr: {expr: EBlock(exprs.slice(i)), pos: Context.currentPos()}});
+							}
+						default:
+					}
+				}
+				body.expr = EBlock(exprs);
+			default:
+		}
+		for (i in 0...cases.length) {
+			switch cases[i].expr.expr {
+				case EBlock(exprs):
+					final name = i == cases.length - 1 ? macro "" : cases[i + 1].values[0];
+					exprs.push(macro $i{selectionName} = $name);
+					cases[i].expr.expr = EBlock(exprs);
+				default:
+			}
+		}
+		var switchStmt:Expr = {expr: ESwitch(macro $i{selectionName}, cases, null), pos: Context.currentPos()};
+		final v:Expr = {expr: EVars(vars), pos: Context.currentPos()};
+		final e = macro {
+			var $selectionName = "";
+			var $exitName = false;
+			$v;
+			do {
+				$i{exitName} = false;
+				$switchStmt;
+			} while ($i{selectionName} != "");
+		};
+		trace(new haxe.macro.Printer().printExpr(e));
+		return e;
+	}
+
 	public static macro function intEnum():Array<Field> {
 		switch (Context.getLocalClass().get().kind) {
 			case KAbstractImpl(_.get() => {type: TAbstract(_.get() => {name: "Int"}, _)}):

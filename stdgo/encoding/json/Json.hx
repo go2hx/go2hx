03f8b36667488d0af7de5b557211128d53d98800
PXshadow <joeyprather8@gmail.com>
2021-08-22 19:11:43 -0700

    remove stdgo.internal.ErrorReturn refrences

--- stdgo/encoding/json/Json.hx
+++ stdgo/encoding/json/Json.hx
@@ -3,7 +3,6 @@ package stdgo.encoding.json;
 import stdgo.GoString;
 import stdgo.Slice;
 import stdgo.StdGoTypes;
-import stdgo.internal.ErrorReturn;
 
 function marshal(v:AnyInterface):{v0:Slice<GoByte>, v1:Error} {
 	return {v0: ((haxe.Json.stringify(v.value) : GoString) : Slice<GoByte>), v1: null};
--- stdgo/io/ioutil/Ioutil.hx
+++ stdgo/io/ioutil/Ioutil.hx
@@ -2,15 +2,13 @@ package stdgo.io.ioutil;
 
 import haxe.io.Bytes;
 import haxe.io.Path;
-import stdgo.Io.Reader;
-import stdgo.Os;
 import stdgo.StdGoTypes;
-import stdgo.internal.ErrorReturn;
-import stdgo.internal.ErrorReturn;
+import stdgo.io.Io.Reader;
+import stdgo.os.Os;
 import sys.FileSystem;
 import sys.io.File;
 
-function readAll(r:stdgo.Io.Reader)
+function readAll(r:Reader)
 	return stdgo.io.Io.readAll(r);
 
 function readFile(filename:GoString) {}
@@ -24,14 +22,14 @@ function writeFile(filename:GoString, data:Bytes, ?perm:GoInt):Error {
 	}
 }
 
-function readDir(dirname:GoString):ErrorReturn<Slice<FileInfo>> {
+function readDir(dirname:GoString):{v0:Slice<FileInfo>, ?v1:Error} {
 	dirname = Path.addTrailingSlash(dirname);
 	try {
 		var array:Array<FileInfo> = [];
 		for (path in FileSystem.readDirectory(dirname)) {}
-		return {value: new Slice(...array)};
+		return {v0: new Slice(...array)};
 	} catch (e) {
-		return {value: null, error: cast e};
+		return {v0: new Slice().nil(), v1: stdgo.errors.Errors.new_(e.message)};
 	}
 }
 

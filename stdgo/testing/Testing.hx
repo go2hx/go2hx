2766813e02e526a4ec8a74ae70b690db1186f249
PXshadow <9813078+PXshadow@users.noreply.github.com>
2021-09-23 19:19:48 -0700

    add more std testing types

--- stdgo/testing/Testing.hx
+++ stdgo/testing/Testing.hx
@@ -1,10 +1,144 @@
 package stdgo.testing;
 
 import haxe.Rest;
-import stdgo.StdGoTypes.AnyInterface;
-import stdgo.StdGoTypes.GoInt;
-import stdgo.StdGoTypes.StructType;
+import stdgo.StdGoTypes;
+import stdgo.reflect.Reflect.GoType;
 import stdgo.testing.internal.testdeps.Testdeps.TestDeps;
+
+@:structInit
+class InternalFuzzTarget implements StructType {
+	public function new() {}
+
+	public function __underlying__():AnyInterface
+		return null;
+}
+
+class BenchmarkResult {
+	public var n:GoInt = 0;
+	public var t:Dynamic = null;
+	public var bytes:GoInt64 = 0;
+	public var memAllocs:GoUInt64 = 0;
+	public var memBytes:GoUInt64 = 0;
+	public var extra = new GoMap<GoString, GoFloat64>(new stdgo.reflect.Reflect._Type(mapType(basic(string_kind), basic(float64_kind)))).nil();
+
+	public function new(?n, ?t, ?bytes, ?memAllocs, ?memBytes, ?extra) {
+		stdgo.internal.Macro.initLocals();
+	}
+
+	public function benchmark(f:Pointer<B>->Void):BenchmarkResult {
+		return null;
+	}
+
+	public function allocedBytesPerOp():GoInt64
+		return 0;
+
+	public function allocsPerOp():GoInt64
+		return 0;
+
+	public function memString():GoString
+		return "";
+
+	public function toString():GoString
+		return "";
+}
+
+@:structInit
+class PB implements StructType {
+	public function new() {}
+
+	public function next():Bool
+		return false;
+
+	public function __underlying__():AnyInterface
+		return null;
+}
+
+@:structInit
+class B implements StructType {
+	public var n:GoInt = 0;
+
+	var skipBool:Bool = false;
+	var failBool:Bool = false;
+	var output:StringBuf = null;
+
+	public function new(?n:GoInt) {
+		if (n != null)
+			this.n = n;
+	}
+
+	public function cleanup(f:Void->Void) {}
+
+	public function error(args:Rest<AnyInterface>) {
+		stdgo.fmt.Fmt.println(args);
+		fail();
+	}
+
+	public function errorf(format:GoString, args:Rest<AnyInterface>) {
+		stdgo.fmt.Fmt.printf(format, ...args);
+		stdgo.fmt.Fmt.println();
+		fail();
+	}
+
+	public function fail() // marks the function as failed byt continues the execution
+		failBool = true;
+
+	public function failNow() { // FailNow marks the function as having failed and stops its execution by calling runtime.Goexit
+		failBool = true;
+		// end current process
+		throw "fail";
+	}
+
+	public function failed():Bool
+		return failBool;
+
+	public function fatal(args:Rest<AnyInterface>) {}
+
+	public function fatalf(format:GoString, args:Rest<AnyInterface>) {}
+
+	public function helper() {}
+
+	public function log(args:Rest<AnyInterface>) {} // Log formats its arguments using default formatting, analogous to Println, and records the text in the error log. For tests, the text will be printed only if the test fails or the -test.v flag is set. For benchmarks, the text is always printed
+
+	public function logf(format:GoString, args:Rest<AnyInterface>) {}
+
+	public function name():GoString {
+		return "";
+	}
+
+	public function reportAllocs() {}
+
+	public function reportMetric(n:GoFloat64, uint:GoString) {}
+
+	public function runParrallel(body:Pointer<PB>->Void) {
+		return null;
+	}
+
+	public function setBytes(n:GoInt64) {}
+
+	public function setParallelism(p:GoInt) {}
+
+	public function setEnv(key:GoString, value:GoString) {}
+
+	public function skip(args:Rest<AnyInterface>) {}
+
+	public function skipNow() {}
+
+	public function skipf(format:GoString, args:Rest<AnyInterface>) {}
+
+	public function skipped():Bool
+		return skipBool;
+
+	public function startTimer() {}
+
+	public function stopTimer() {}
+
+	public function tempDir():GoString {
+		return "";
+	}
+
+	public function __underlying__():AnyInterface
+		return null;
+}
 
 @:structInit
 class T_ implements StructType {
@@ -139,13 +273,18 @@ class M implements StructType {
 
 @:structInit
 class InternalBenchmark implements StructType {
-	public function new() {}
+	public var name:GoString = "";
+	public var f:Pointer<B>->Void = null;
+
+	public function new(?name, ?f) {
+		stdgo.internal.Macro.initLocals();
+	}
 
 	public function __underlying__():AnyInterface
 		return null;
 
 	public function __copy__()
-		return new InternalBenchmark();
+		return new InternalBenchmark(name, f);
 }
 
 @:structInit

ee11f1242c8d8d68cadfb11b9f99f39a61a02321
Joey <9813078+PXshadow@users.noreply.github.com>
2021-12-09 05:39:04 -0800

    simplify RunRnd using Main.run instead of compile

--- rnd/RunRnd.hx
+++ rnd/RunRnd.hx
@@ -5,37 +5,33 @@ import shared.Util;
 
 function main() {
 	final testBool = false;
-	Main.setup(0, 1, () -> {
-		Main.onComplete = (modules, data) -> {
-			if (modules.length == 0)
-				throw "no exported path";
-			final mainPath = mainPath(modules);
-			final command = 'haxe -cp golibs extraParams.hxml -main $mainPath -hl runrnd.hl';
-			Sys.println(command);
-			Sys.command(command);
-			final runCommand = 'hl runrnd.hl';
-			Sys.println(runCommand);
-			Sys.command(runCommand);
-			Sys.println("\n~~~~~~~~~~~~expected~~~~~~~~~~~~");
-			var command = testBool ? "test" : "run";
-			final wasm = false;
-			if (wasm) {
-				Sys.putEnv("GOOS", "js");
-				Sys.putEnv("GOARCH", "wasm");
-				final run = systemName == "Windows" ? "bash" : "sh";
-				if (command == "run")
-					command = "build";
-				Sys.command('go $command -o rnd.wasm ./rnd && $run go_js_wasm_exec rnd.wasm');
-			} else {
-				Sys.command('go $command ./rnd');
-			}
-			Main.close();
-		};
-		final args = ["-log", "./rnd", Sys.getCwd()];
-		if (testBool)
-			args.unshift("-test");
-		Sys.println("compile: " + Main.compile(args));
-	});
-	while (true)
-		Main.update();
+	Main.onComplete = (modules, data) -> {
+		if (modules.length == 0)
+			throw "no exported path";
+		final mainPath = mainPath(modules);
+		final command = 'haxe -cp golibs extraParams.hxml -main $mainPath -hl runrnd.hl';
+		Sys.println(command);
+		Sys.command(command);
+		final runCommand = 'hl runrnd.hl';
+		Sys.println(runCommand);
+		Sys.command(runCommand);
+		Sys.println("\n~~~~~~~~~~~~expected~~~~~~~~~~~~");
+		var command = testBool ? "test" : "run";
+		final wasm = false;
+		if (wasm) {
+			Sys.putEnv("GOOS", "js");
+			Sys.putEnv("GOARCH", "wasm");
+			final run = systemName == "Windows" ? "bash" : "sh";
+			if (command == "run")
+				command = "build";
+			Sys.command('go $command -o rnd.wasm ./rnd && $run go_js_wasm_exec rnd.wasm');
+		} else {
+			Sys.command('go $command ./rnd');
+		}
+		Main.close();
+	};
+	final args = ["-log", "./rnd", Sys.getCwd()];
+	if (testBool)
+		args.unshift("-test");
+	Main.run(args);
 }

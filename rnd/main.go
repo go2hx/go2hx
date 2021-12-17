f6261da9fc479940146fa640f6024a921fb2e38e
Joey <9813078+PXshadow@users.noreply.github.com>
2021-12-17 12:25:56 +0100

    rnd math test

--- rnd/main.go
+++ rnd/main.go
@@ -1,14 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
+	input := -2.7688005719200159e-01
+	output := math.Ceil(input)
+	expected := math.Copysign(0, -1)
+	fmt.Println(alike(output, expected))
+}
 
-	var a, b, c int
+func Signbit(x float64) bool {
+	fmt.Println(math.Float64bits(x))
+	return math.Float64bits(x)&(1<<63) != 0
+}
 
-	a = 10
-	b = 20
-	c = a + b
-
-	fmt.Printf("result： a = %d, b = %d and c = %d\n", a, b, c) //will trace ArrayBuffer in haxe
+func alike(a, b float64) bool {
+	switch {
+	case math.IsNaN(a) && math.IsNaN(b):
+		return true
+	case a == b:
+		return Signbit(a) == Signbit(b)
+	}
+	return false
 }

2873516d76581679ef4b580b60bc54eef5a7a7b5
Joey <9813078+PXshadow@users.noreply.github.com>
2021-12-21 23:49:33 +0100

    remove internal math

--- stdgo/internal/Math.hx
+++
@@ -1,9 +0,0 @@
-package stdgo.internal;
-
-import stdgo.StdGoTypes;
-import stdgo.internal.injector.math.Math.HaxeMath.*;
-
-function copysign(_x:GoFloat, _y:GoFloat):GoFloat {
-	final _sign:GoUnTypedInt = ((1 : GoUnTypedInt)) << ((63 : GoUnTypedInt));
-	return float64frombits((float64bits(_x) & (_sign ^ ((-1 : GoUnTypedInt)))) | (float64bits(_y) & _sign));
-}

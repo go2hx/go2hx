c94f6f12b04b4b997de90722abcf1a151b9d682c
Joey <9813078+PXshadow@users.noreply.github.com>
2022-02-15 08:07:31 -0800

    atomic std work

--- stdgo/sync/atomic/Atomic.hx
+++ stdgo/sync/atomic/Atomic.hx
@@ -5,31 +5,37 @@ import haxe.Int64;
 import stdgo.StdGoTypes;
 
 function addInt32(addr:Pointer<GoInt32>, delta:GoInt32):GoInt32 {
-	return addr._value_ + delta;
+	return addr.value + delta;
 }
 
 function addInt64(addr:Pointer<GoInt64>, delta:GoInt64):GoInt64 {
-	return addr._value_ + delta;
+	return addr.value + delta;
 }
 
-function addUInt32(addr:Pointer<GoUInt32>, delta:GoUInt32):GoUInt32 {
-	return addr._value_ + delta;
+function addUint32(addr:Pointer<GoUInt32>, delta:GoUInt32):GoUInt32 {
+	return addr.value + delta;
 }
 
-function addUInt64(addr:Pointer<GoUInt64>, delta:GoUInt64):GoUInt64 {
-	return addr._value_ + delta;
+function addUint64(addr:Pointer<GoUInt64>, delta:GoUInt64):GoUInt64 {
+	return addr.value + delta;
 }
 
-function compareAndSwapUInt32(addr:Pointer<GoUInt32>, old:GoUInt32, new_:GoUInt32):Bool {
-	if (addr._value_ == old) {
-		addr._value_ = new_;
+function loadUint32(addr:Pointer<GoUInt32>):GoUInt32
+	return addr.value;
+
+function loadUint64(addr:Pointer<GoUInt64>):GoUInt64
+	return addr.value;
+
+function compareAndSwapUint32(addr:Pointer<GoUInt32>, old:GoUInt32, new_:GoUInt32):Bool {
+	if (addr.value == old) {
+		addr.value = new_;
 		return true;
 	}
 	return false;
 }
 
 function swapUInt32(addr:Pointer<GoUInt32>, new_:GoUInt32):GoUInt32 {
-	var old = addr._value_;
-	addr._value_ = new_;
+	var old = addr.value;
+	addr.value = new_;
 	return old;
 }

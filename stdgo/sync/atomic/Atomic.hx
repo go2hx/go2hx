8c857bc26516b2181886704e59f2d9d6e9a40e62
Joey <9813078+PXshadow@users.noreply.github.com>
2022-03-14 17:06:52 -0800

    stdgo.sync.atomic add mutexs in unprotected funcs

--- stdgo/sync/atomic/Atomic.hx
+++ stdgo/sync/atomic/Atomic.hx
@@ -4,9 +4,7 @@ import haxe.Int32;
 import haxe.Int64;
 import stdgo.StdGoTypes;
 
-#if (target.threaded)
 var mutex = new sys.thread.Mutex();
-#end
 
 function addInt32(addr:Pointer<GoInt32>, delta:GoInt32):GoInt32 {
 	mutex.acquire();
@@ -36,17 +34,28 @@ function addUint64(addr:Pointer<GoUInt64>, delta:GoUInt64):GoUInt64 {
 	return value;
 }
 
-function loadUint32(addr:Pointer<GoUInt32>):GoUInt32
-	return addr.value;
+function loadUint32(addr:Pointer<GoUInt32>):GoUInt32 {
+	mutex.acquire();
+	final value = addr.value;
+	mutex.release();
+	return value;
+}
 
-function loadUint64(addr:Pointer<GoUInt64>):GoUInt64
-	return addr.value;
+function loadUint64(addr:Pointer<GoUInt64>):GoUInt64 {
+	mutex.acquire();
+	final value = addr.value;
+	mutex.release();
+	return value;
+}
 
 function compareAndSwapUint32(addr:Pointer<GoUInt32>, old:GoUInt32, new_:GoUInt32):Bool {
+	mutex.acquire();
 	if (addr.value == old) {
 		addr.value = new_;
+		mutex.release();
 		return true;
 	}
+	mutex.release();
 	return false;
 }
 

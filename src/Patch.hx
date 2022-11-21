fd634cd642a743ef824932e1d1e40079ebdab9db
Joey <9813078+PXshadow@users.noreply.github.com>
2022-11-21 18:02:40 +0100

    Patch: add reflect Kind string func

--- src/Patch.hx
+++ src/Patch.hx
@@ -248,6 +248,40 @@ final list = [
 				new stdgo.internal.reflect.Reflect._Type(basic(unsafepointer_kind)));
 		// set internal Type
 		return new stdgo.internal.reflect.Reflect._Type_asInterface(Go.pointer(_i.type), _i.type);
+	},
+	"reflect.Kind:string" => macro {
+		var idx:Int = _k.toBasic();
+		trace(idx);
+		return switch idx {
+			case 0: "invalid";
+			case 1: "bool";
+			case 2: "int";
+			case 3: "int8";
+			case 4: "int16";
+			case 5: "int32";
+			case 6: "int64";
+			case 7: "uint";
+			case 8: "uint8";
+			case 9: "uint16";
+			case 10: "uint32";
+			case 11: "uint64";
+			case 12: "uintptr";
+			case 13: "float32";
+			case 14: "float64";
+			case 15: "complex64";
+			case 16: "complex128";
+			case 17: "array";
+			case 18: "chan";
+			case 19: "func";
+			case 20: "interface";
+			case 21: "map";
+			case 22: "ptr";
+			case 23: "slice";
+			case 24: "string";
+			case 25: "struct";
+			case 26: "unsafe.Pointer";
+			default: throw "unknown kind string: " + idx;
+		}
 	}
 ];
 

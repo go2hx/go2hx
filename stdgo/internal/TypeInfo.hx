75a0f82fb2a45b8dac9f9b1fb88c291d3a5108aa
Joey <9813078+PXshadow@users.noreply.github.com>
2023-01-03 12:43:11 +0100

    stdgo/internal/TypeInfo: hold type info

---
+++ stdgo/internal/TypeInfo.hx
@@ -0,0 +1,15 @@
+package stdgo.internal;
+class TypeInfo {
+    public static var names:TypeInternalMap = std.Type.createInstance(std.Type.resolveClass('TypeInfoData'),[]).names;
+
+}
+
+private abstract TypeInternalMap(Map<String,stdgo.internal.reflect.Reflect.GoType>) from Map<String,stdgo.internal.reflect.Reflect.GoType> {
+    @:op([])
+    private function get(s:String) {
+        final value = this[s];
+        if (value == null)
+            trace("null value: " + s);
+        return value;
+    }
+}

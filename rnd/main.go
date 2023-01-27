a296077fedb1fc313eb2eb1fe5ae2e3c2a99d12d
Joey <9813078+PXshadow@users.noreply.github.com>
2023-01-27 17:50:59 +0100

    rnd/main.go: add macro-in-macro error test case

--- rnd/main.go
+++ rnd/main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+type I[T float32 | float64] int
+
+func (i I[t]) String() string { return fmt.Sprintf("<%d>", int(i)) }
+
+type B struct {
+	I I[float32]
+	j int
+}
+
+type C struct {
+	i int
+	B
+}
 
 func main() {
-	fmt.Println("hello world")
+	fmt.Println(fmt.Errorf("%+v", C{1, B{2, 3}}))
 }

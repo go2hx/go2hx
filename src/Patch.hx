b853eb86a77064cb2c2181c147c6a8070189b890
Joey <9813078+PXshadow@users.noreply.github.com>
2023-02-15 21:24:55 +0100

    Patch: skip testGenericTriedBuilding test because of seg fault

--- src/Patch.hx
+++ src/Patch.hx
@@ -950,6 +950,7 @@ final list = [
 ];
 
 final skipTargets = [
+	"strings:testGenericTrieBuilding" => [], // Segmentation fault (core dumped)
 	"strings:testWriteTo" => [], // Segmentation fault (core dumped)
 	"strings:testReaderZero" => [], // Segmentation fault (core dumped)
 	"unicode.utf8:testReaderCopyNothing" => [], // Segmentation fault (core dumped)

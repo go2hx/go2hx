c9ee6316498085a110fec57f461e3de6420a69b9
Joey <9813078+PXshadow@users.noreply.github.com>
2023-09-12 06:09:40 +0000

    codespaces: update

--- codespaces.sh
+++ codespaces.sh
@@ -1,7 +1,8 @@
-
+curl -OL https://golang.org/dl/go1.21.1.linux-amd64.tar.gz
+sudo  rm -rf /usr/local/go && tar -xzf go1.21.1.linux-amd64.tar.gz -C /usr/local
+export PATH=$PATH:/usr/local/go/bin
+sudo nano ~/.profile
 npm i lix -g
 npx lix download
 npx haxelib dev go2hx .
 npx haxelib run go2hx ./rnd
-haxe --run ModGo.hx
-go version

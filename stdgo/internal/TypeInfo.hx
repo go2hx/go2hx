55af9abd265d681fbe359c7ccb8e16b1299d3d32
Joey <9813078+PXshadow@users.noreply.github.com>
2024-01-19 15:21:12 +0100

    TypeInfo: handle both hxb and non TypeInfoData

--- stdgo/internal/TypeInfo.hx
+++ stdgo/internal/TypeInfo.hx
@@ -1,12 +1,17 @@
 package stdgo.internal;
 class TypeInfo {
-    public static var names:TypeInternalMap = std.Type.createInstance(std.Type.resolveClass('TypeInfoData_go2hx_'),[]).names;
-    /*public static var names:TypeInternalMap = try std.Type.createInstance(std.Type.resolveClass('TypeInfoData_go2hx_'),[]).names catch(_) {
-        null;
-    };*/
-
+    public static var namesHxb:TypeInternalMap = try std.Type.createInstance(std.Type.resolveClass('TypeInfoData_go2hx_1'),[]).names catch(_) {
+        cast [];
+    };
+    public static var names:TypeInternalMap = {
+        var names:TypeInternalMap = std.Type.createInstance(std.Type.resolveClass('TypeInfoData_go2hx_null'),[]).names;
+        for (k in namesHxb.keys())
+            names[k] = namesHxb[k];
+        names;
+    };
 }
 
+@:forward
 private abstract TypeInternalMap(Map<String,stdgo.internal.reflect.Reflect.GoType>) from Map<String,stdgo.internal.reflect.Reflect.GoType> {
     @:op([])
     private function get(s:String) {
@@ -16,5 +21,11 @@ private abstract TypeInternalMap(Map<String,stdgo.internal.reflect.Reflect.GoTyp
         if (value == null)
             trace("null value: " + s);
         return value;
+    }
+    @:op([])
+    private function set(s:String, value:stdgo.internal.reflect.Reflect.GoType) {
+        if (this == null)
+            throw "TypeInternalMap null";
+        this[s] = value;
     }
 }

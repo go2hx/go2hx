b9e73f3fa9603f7a9bb0b1bc980df62aa3b8e1c4
Joey <9813078+PXshadow@users.noreply.github.com>
2024-01-19 14:27:14 +0100

    rnd: simple hello world

--- rnd/main.go
+++ rnd/main.go
@@ -1,23 +1,7 @@
 package main
 
-import (
-	"fmt"
-)
-
-type I[T float32 | float64] int
-
-func (i I[t]) String() string { return fmt.Sprintf("<%d>", int(i)) }
-
-type B struct {
-	I I[float32]
-	j int
-}
-
-type C struct {
-	i int
-	B
-}
+import "fmt"
 
 func main() {
-	fmt.Println(fmt.Errorf("%+v", C{1, B{2, 3}}))
+	fmt.Println("hello world")
 }

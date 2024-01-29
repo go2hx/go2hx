65b0970fd6ec6fc97bf54a222a2a3ae6f3c22faa
Joey <9813078+PXshadow@users.noreply.github.com>
2024-01-29 10:21:45 +0100

    RunRnd: hxb define

--- rnd/RunRnd.hx
+++ rnd/RunRnd.hx
@@ -6,25 +6,36 @@ import src.Util;
 
 function main() {
 	final testBool = false;
+	final hxbBool = Compiler.getDefine("hxb") != null;
 	Main.onComplete = (modules, data) -> {
 		if (modules.length == 0)
 			throw "no exported path";
 		final mainPath = mainPath(modules);
 		if (Compiler.getDefine("rnd_cpp") != null) {
-			final command = 'haxe -cp golibs extraParams.hxml -main $mainPath --cpp bin/cpp -lib hxcpp -lib hxcpp-debug-server';
+			final command = 'haxe -cp golibs extraParams.hxml -main $mainPath --cpp bin/cpp -lib hxcpp -lib hxcpp-debug-server' + (hxbBool ? " --hxb-lib go2hx.zip" : "");
 			Sys.println(command);
 			Sys.command(command);
 			final command = './bin/cpp/rnd';
 			Sys.println(command);
 			Sys.command(command);
 		} else if (Compiler.getDefine("rnd_interp") != null) {
-			final command = 'haxe -cp golibs extraParams.hxml -main $mainPath --interp';
+			final command = 'haxe -cp golibs extraParams.hxml -main $mainPath --interp' + (hxbBool ? " --hxb-lib go2hx.zip" : "");
 			Sys.println(command);
 			Sys.command(command);
 		}else{
-			final command = 'haxe -cp golibs extraParams.hxml -main $mainPath -hl runrnd.hl';
+			final command = 'haxe -cp golibs extraParams.hxml -main $mainPath -hl runrnd.hl'  + (hxbBool ? " --hxb-lib go2hx.zip" : "");
 			Sys.println(command);
-			Sys.command(command);
+			final proc = new sys.io.Process(command);
+			final exitCode = proc.exitCode();
+			if (exitCode != 0) {
+				Sys.println(proc.stderr.readAll().toString());
+				proc.close();
+				Main.close();
+				return;	
+			}else{
+				Sys.println(proc.stdout.readAll().toString());
+			}
+			proc.close();
 			final profileBool = Compiler.getDefine("profile") != null;
 			final command = profileBool ? "hl --profile 10000 runrnd.hl" : "hl runrnd.hl";
 			Sys.println(command);
@@ -52,7 +63,7 @@ function main() {
 		}
 		Main.close();
 	};
-	final args = ["-norun", "-log", "-stack", "./rnd", Sys.getCwd()];
+	final args = ["-norun", "-log", "./rnd", Sys.getCwd()];
 	// args.unshift("--extern");
 	if (testBool)
 		args.unshift("-test");

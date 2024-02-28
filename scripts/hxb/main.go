a2c6eeb62577b16c76217256dcd858722eb5db64
PXshadow <PXshadow@users.noreply.github.com>
2024-02-28 20:01:08 +0000

    stdgo automatic compile

--- scripts/hxb/main.go
+++ scripts/hxb/main.go
@@ -3,21 +3,23 @@ package main
 import (
     _ "bytes"
     _ "encoding/base64"
-    _ "encoding/base64"
     _ "encoding/hex"
     _ "errors"
+    _ "hash/adler32"
     _ "math"
     _ "math/bits"
     _ "path"
     _ "sort"
     _ "strconv"
     _ "strings"
+    _ "text/scanner"
     _ "unicode"
     _ "unicode/utf16"
     _ "unicode/utf8"
 )
 
 func main() {
-    println("hello hxb")
+    _ = 1
+    println(0)
 }
     

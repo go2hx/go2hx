249435eeda50e1323de6bc4c160a69755aa8bcac
PXshadow <PXshadow@users.noreply.github.com>
2024-04-13 01:39:41 +0000

    stdgo automatic compile

--- stdgo/_internal/internal/abi/Abi.hx
+++ stdgo/_internal/internal/abi/Abi.hx
@@ -122,9 +122,9 @@ var _kindNames : stdgo.Slice<stdgo.GoString> = {
         s;
     };
 @:structInit @:using(stdgo._internal.internal.abi.Abi.RegArgs_static_extension) class RegArgs {
-    public var ints : stdgo.GoArray<stdgo.GoUIntptr> = new stdgo.GoArray<stdgo.GoUIntptr>(0, 0, ...[for (i in 0 ... 0) (0 : stdgo.GoUIntptr)]);
-    public var floats : stdgo.GoArray<stdgo.GoUInt64> = new stdgo.GoArray<stdgo.GoUInt64>(0, 0, ...[for (i in 0 ... 0) (0 : stdgo.GoUInt64)]);
-    public var ptrs : stdgo.GoArray<stdgo._internal.unsafe.Unsafe.UnsafePointer> = new stdgo.GoArray<stdgo._internal.unsafe.Unsafe.UnsafePointer>(0, 0, ...[for (i in 0 ... 0) @:default_value null]);
+    public var ints : stdgo.GoArray<stdgo.GoUIntptr> = new stdgo.GoArray<stdgo.GoUIntptr>(0, 0);
+    public var floats : stdgo.GoArray<stdgo.GoUInt64> = new stdgo.GoArray<stdgo.GoUInt64>(0, 0);
+    public var ptrs : stdgo.GoArray<stdgo._internal.unsafe.Unsafe.UnsafePointer> = new stdgo.GoArray<stdgo._internal.unsafe.Unsafe.UnsafePointer>(0, 0);
     public var returnIsPtr : stdgo._internal.internal.abi.Abi.IntArgRegBitmap = new stdgo._internal.internal.abi.Abi.IntArgRegBitmap(0, 0, ...[for (i in 0 ... 0) (0 : stdgo.GoUInt8)]);
     public function new(?ints:stdgo.GoArray<stdgo.GoUIntptr>, ?floats:stdgo.GoArray<stdgo.GoUInt64>, ?ptrs:stdgo.GoArray<stdgo._internal.unsafe.Unsafe.UnsafePointer>, ?returnIsPtr:stdgo._internal.internal.abi.Abi.IntArgRegBitmap) {
         if (ints != null) this.ints = ints;

e2cf506d69c262e96a4366774666c4a8f104a8e2
Joey <9813078+PXshadow@users.noreply.github.com>
2024-09-21 20:49:39 +0200

    tests/unit: add tagBootstrap to check if bootstrap build is configured properly

---
+++ tests/unit/tagBootstrap.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"math/bits"
+	"runtime"
+)
+
+const (
+	divZeroErrorStr  = "runtime error: integer divide by zero"
+	overflowErrorStr = "runtime error: integer overflow"
+)
+
+func main() {
+	runBits()
+	runInternal()
+}
+
+func runBits() {
+	defer func() {
+		if err := recover(); err == nil {
+			println("Div should have panicked when y<=hi")
+		} else if e, ok := err.(runtime.Error); !ok || e.Error() != overflowErrorStr {
+			println(e)
+			fmt.Printf("Div expected panic: %q, got: %q ", overflowError, e.Error())
+		}
+	}()
+
+	q, r := bits.Div(1, 0, 1)
+	_, _ = q, r
+}
+
+func runInternal() {
+	defer func() {
+		if err := recover(); err == nil {
+			println("Div should have panicked when y<=hi")
+		} else if e, ok := err.(runtime.Error); !ok || e.Error() != overflowErrorStr {
+			println(e)
+			fmt.Printf("Div expected panic: %q, got: %q ", overflowError, e.Error())
+		}
+	}()
+	panic(overflowError)
+}
+
+type errorString string
+
+func (e errorString) RuntimeError() {}
+
+func (e errorString) Error() string {
+	return "runtime error: " + string(e)
+}
+
+var overflowError = error(errorString("integer overflow"))
+
+var divideError = error(errorString("integer divide by zero"))

3750343b7d99eda4598f8d182abdfb1b0b374117
Joey <9813078+PXshadow@users.noreply.github.com>
2024-09-21 22:40:50 +0200

    stdgo/Error: simplify divideError and overflowError by using bits bootstrapped errors

--- stdgo/Error.hx
+++ stdgo/Error.hx
@@ -9,50 +9,9 @@ import stdgo.Pointer;
 import stdgo.Slice;
 
 
-private var _shiftError:Error = Go.asInterface((Go.str("negative shift amount") : T_errorString));
-private var _divideError:Error = Go.asInterface((Go.str("integer divide by zero") : T_errorString));
-private var _overflowError:Error = Go.asInterface((Go.str("integer overflow") : T_errorString));
-private var _floatError:Error = Go.asInterface((Go.str("floating point error") : T_errorString));
-private var _memoryError:Error = Go.asInterface((Go.str("invalid memory address or nil pointer dereference") : T_errorString));
+private var _divideError:Error = stdgo._internal.math.bits.Bits__divideError._divideError;
+private var _overflowError:Error = stdgo._internal.math.bits.Bits__overflowError._overflowError;
 
 typedef Error = StructType & {
 	public dynamic function error():GoString;
 };
-
-/**
-	// An errorString represents a runtime error described by a single string.
-**/
-@:named @:using(Error.T_errorString_static_extension) private typedef T_errorString = GoString;
-
-class T_errorString_asInterface {
-	@:keep
-	public dynamic function error():GoString
-		return __self__.value.error();
-
-	@:keep
-	public dynamic  function runtimeError():Void
-		__self__.value.runtimeError();
-
-	public function new(__self__, __type__) {
-		this.__self__ = __self__;
-		this.__type__ = __type__;
-	}
-
-	public function __underlying__()
-		return new AnyInterface((__type__.kind() == stdgo._internal.internal.reflect.Reflect.KindType.pointer
-			&& !stdgo._internal.internal.reflect.Reflect.isReflectTypeRef(__type__)) ? (__self__ : Dynamic) : (__self__.value : Dynamic),
-			__type__);
-
-	var __self__:Pointer<T_errorString>;
-	var __type__:stdgo._internal.internal.reflect.Reflect._Type;
-}
-
-@:keep @:allow(Error.T_errorString_asInterface) class T_errorString_static_extension {
-	@:keep
-	static public function error(_e:T_errorString):GoString {
-		return ("runtime error: " : String) + (_e : String);
-	}
-
-	@:keep
-	static public function runtimeError(_e:T_errorString):Void {}
-}

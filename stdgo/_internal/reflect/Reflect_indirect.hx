beeb17145c722850687fc4a34da1064471b22327
PXshadow <PXshadow@users.noreply.github.com>
2024-09-30 19:51:45 +0000

    stdgo automatic compile

--- stdgo/_internal/reflect/Reflect_indirect.hx
+++ stdgo/_internal/reflect/Reflect_indirect.hx
@@ -1,6 +1,6 @@
 package stdgo._internal.reflect;
 function indirect(_v:stdgo._internal.reflect.Reflect_Value.Value):stdgo._internal.reflect.Reflect_Value.Value {
-        if (_v.kind() != pointer) {
+        if (_v.kind() != stdgo._internal.reflect.Reflect_pointer.pointer) {
             return _v;
         } else {
             return _v.elem();
--- stdgo/_internal/time/Time__runtimeNano.hx
+++ stdgo/_internal/time/Time__runtimeNano.hx
@@ -1,5 +1,7 @@
 package stdgo._internal.time;
 function _runtimeNano():stdgo.GoInt64 {
-        final x = ((std.Sys.time() * 1000000 * 1000) - std.Date.now().getTimezoneOffset() * 60000000000 : stdgo.GoInt64);
-        return x;
+        #if sys {
+            return ((std.Sys.time() * 1000000 * 1000) - std.Date.now().getTimezoneOffset() * 60000000000 : stdgo.GoInt64);
+        } #else null #end;
+        return 0;
     }

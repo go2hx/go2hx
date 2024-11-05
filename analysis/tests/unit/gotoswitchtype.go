50618b34933353af45fa7a3415d1a70230de2acb
Joey <9813078+PXshadow@users.noreply.github.com>
2024-11-05 05:07:13 +0100

    analysis/tests/unit: add new gotoswitchtype test

---
+++ analysis/tests/unit/gotoswitchtype.go
@@ -0,0 +1,22 @@
+package main
+
+import "strings"
+
+func main() {
+	f(0)
+	f(0.0)
+	f("hi")
+}
+
+func f(a any) {
+	switch a := a.(type) {
+	case string:
+		println(len(strings.Split(a, "")))
+	case int:
+		println(a + 10)
+	case float64:
+		println(a + 1.2)
+	}
+	goto JUMP
+JUMP:
+}

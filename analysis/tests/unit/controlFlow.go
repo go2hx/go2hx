4a639c019782b79b5d0b2d801bc2f1e385098080
Joey <9813078+PXshadow@users.noreply.github.com>
2024-11-09 00:44:13 +0100

    analysis/tests/unit: add controlFlow and controlFlow2 tests

---
+++ analysis/tests/unit/controlFlow.go
@@ -0,0 +1,72 @@
+package main
+
+func main() {
+	consumed, err := unmarshalPath()
+	println(consumed, err == nil)
+}
+
+type Token struct{}
+
+func (Token) Token() (any, error) {
+	return 0, nil
+}
+
+func unmarshalPath() (consumed bool, err error) {
+	recurse := false
+	fields := []int{0, 1, 2}
+	parents := []int{1, 0, 3}
+Loop:
+	for i := range fields {
+		finfo := i == 1
+		if finfo {
+			continue
+		}
+		for j := range parents[:len(parents)-1] {
+			if parents[j] != fields[j+1] {
+				continue Loop
+			}
+		}
+		/*if len(finfo.parents) > len(parents) && finfo.parents[len(parents)] == start.Name.Local {
+			// It's a prefix for the field. Break and recurse
+			// since it's not ok for one field path to be itself
+			// the prefix for another field path.
+			recurse = true
+
+			// We can reuse the same slice as long as we
+			// don't try to append to it.
+			parents = finfo.parents[:len(parents)+1]
+			break
+		}*/
+	}
+	if !recurse {
+		// We have no business with this element.
+		return false, nil
+	}
+	// The element is not a perfect match for any field, but one
+	// or more fields have the path to this element as a parent
+	// prefix. Recurse and attempt to match these.
+	for {
+		var d Token
+		tokType, err := d.Token()
+		if err != nil {
+			return true, err
+		}
+		switch t := tokType.(type) {
+		case int:
+			// the recursion depth of unmarshalPath is limited to the path length specified
+			// by the struct field tag, so we don't increment the depth here.
+			consumed2, err := true, error(nil)
+			if err != nil {
+				return true, err
+			}
+			if !consumed2 {
+				if err := error(nil); err != nil {
+					return true, err
+				}
+			}
+		case string:
+			println(t)
+			return true, nil
+		}
+	}
+}
---
+++ analysis/tests/unit/controlFlow2.go
@@ -0,0 +1,57 @@
+package main
+
+func main() {
+	if f() != 0 || f2() != 1 || f3() != 2 {
+		panic("invalid result")
+	}
+}
+
+func f() int {
+	var b any = "hello"
+	for {
+		switch b.(type) {
+		case int:
+			return 0
+		}
+		b = int(10)
+	}
+	goto test
+test:
+	return 10
+}
+
+func f2() int {
+	for {
+		break
+	}
+	return 1
+}
+
+func f3() int {
+	if true {
+		var b any = "hello"
+		doubleBreak := false
+		for {
+			//println("in for loop")
+			switch b.(type) {
+			case int:
+				doubleBreak = true
+				break
+			}
+			//println(doubleBreak)
+			if doubleBreak {
+				break
+			}
+			b = int(10)
+		}
+		//println("out of for loop")
+		goto test
+		//println("goto middle")
+	test:
+		//println("jump to goto test")
+	}
+	//test2:
+
+	//println("out of if statement")
+	return 2
+}

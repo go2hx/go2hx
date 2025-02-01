2f89a4d85fd03ea3fc812d3b5635a7da1845e3df
Joey <9813078+PXshadow@users.noreply.github.com>
2025-02-01 21:54:42 +0100

    stdgo: manually delete last file case collisions

--- stdgo/_internal/time/Time__stdpm.hx
+++
@@ -1,7 +0,0 @@
-package stdgo._internal.time;
-import stdgo._internal.errors.Errors;
-import stdgo._internal.syscall.Syscall;
-import stdgo._internal.syscall.js.Js;
-import stdgo._internal.internal.itoa.Itoa;
-import stdgo._internal.runtime.Runtime;
-final _stdpm = (534i64 : stdgo.GoUInt64);

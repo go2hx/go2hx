964e7a4328831add0473b033ba5241de8d905b8d
Joey <9813078+PXshadow@users.noreply.github.com>
2025-02-05 15:52:08 +0100

    TypeInfo: remove hxb impl for now, simplify for nodeps use

--- stdgo/_internal/internal/TypeInfo.hx
+++ stdgo/_internal/internal/TypeInfo.hx
@@ -1,14 +1,8 @@
 package stdgo._internal.internal;
 class TypeInfo {
     public static var names:TypeInternalMap = {
-        final names:TypeInternalMap = new TypeInfoData_go2hx_hxb().names;//std.Type.createInstance(std.Type.resolveClass('TypeInfoData_go2hx_'),[]).names;
-        try {
-            final names2:TypeInternalMap = untyped cast std.Type.createInstance(std.Type.resolveClass('TypeInfoData_go2hx_'),[]).names;
-            for (key => value in names2)
-                untyped names[key] = value;
-        }catch(e) {
-            //trace(e);
-        }
+        final names:TypeInternalMap = new TypeInfoData_go2hx_().names;
+        //std.Type.createInstance(std.Type.resolveClass('TypeInfoData_go2hx_'),[]).names;
         names;
     };
 }

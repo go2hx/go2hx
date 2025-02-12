e1a0853195d01330594551df25332860c133b394
Joey <9813078+PXshadow@users.noreply.github.com>
2025-02-12 12:49:29 +0300

    Patch: math:max check that both x and y are 0 before performing

--- src/Patch.hx
+++ src/Patch.hx
@@ -489,8 +489,11 @@ final list = [
 		// special cases
 		if (_x > 0 && !std.Math.isFinite(_x.toBasic()) || _y > 0 && !std.Math.isFinite(_y.toBasic()))
 			return stdgo._internal.math.Math_inf.inf(1);
-		if (_x == 0.0 && !stdgo._internal.math.Math_signbit.signbit(_x) && !stdgo._internal.math.Math_isnan.isNaN(_y) || _y == 0.0 && !stdgo._internal.math.Math_signbit.signbit(_y) && !stdgo._internal.math.Math_isnan.isNaN(_x))
+		if (_x == 0.0 && _y == 0.0 && !stdgo._internal.math.Math_signbit.signbit(_x) && !stdgo._internal.math.Math_isnan.isNaN(_y) 
+		 || _x == 0.0 && _y == 0.0 && !stdgo._internal.math.Math_signbit.signbit(_y) && !stdgo._internal.math.Math_isnan.isNaN(_x)) {
+
 			return 0.0;
+		}
 		if (stdgo._internal.math.Math_isnan.isNaN(_x) || stdgo._internal.math.Math_isnan.isNaN(_y))
 			return stdgo._internal.math.Math_nan.naN();
 		return std.Math.max(_x.toBasic(), _y.toBasic());

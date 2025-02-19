c7243e1b3c625048efaf026c3b4d69e0b8b00433
Joey <9813078+PXshadow@users.noreply.github.com>
2025-02-19 18:20:54 +0300

    tests/interop: more complex tests for interop and interfaces

--- tests/interop/Main.hx
+++ tests/interop/Main.hx
@@ -1,8 +1,11 @@
 package;
 
 import go4hx.tests.interop.Interop;
+//import go4hx.tests.interop.colorful
+import stdgo.encoding.binary.Binary;
 
 function main() {
+    Binary.appendVarint;
     // float32 arg and return
     final value = Interop.float32(10);
     $type(value);
@@ -25,16 +28,32 @@ function main() {
 
     // Interface arg and return
     final i = Interop.interface_(new X());
-    i.run("Haxe");
+    i.run({s: ["Haxe"]});
 
     // Error (Interface) arg and return
     final err = Interop.error(stdgo.errors.Errors.new_("this is an error"));
     trace(err.error().toString());
+
+
+    final i:go4hx.tests.interop.Interop.T_byteOrder = Interop.interfaceByteOrder(new ByteOrder());
+    //trace(Reflect.fields(i));
+    trace(i.appendUint16([0,1,2], 3));
+
+    $type(i);
+}
+
+
+class ByteOrder {
+    public function new() {}
+    public function appendUint16(b:Array<Int>, c:Int):Array<Int> {
+        trace(b,c);
+        return b;
+    }
 }
 
 class X {
     public function new() {}
-    public function run(s:String) {
+    public function run(s:{s: Array<String>}) {
         trace("trigger! " + s);
     }
 }

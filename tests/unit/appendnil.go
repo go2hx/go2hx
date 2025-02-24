e90bd8311f3623362f88c8596ae3ebf5347d0796
Joey <9813078+PXshadow@users.noreply.github.com>
2025-02-24 11:46:23 +0300

    tests/unit: update appendnil test add yaegi-easy issue-1304 test into it

--- tests/unit/appendnil.go
+++ tests/unit/appendnil.go
@@ -1,27 +1,41 @@
-// run
-
-// Copyright 2018 The Go Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
-
-// Verify that we emit a valid type descriptor for
-// a fairly deeply nested type.
-
 package main
 
 import (
+	"fmt"
 	"reflect"
 )
 
+type Node struct {
+	Name  string
+	Alias *Node
+	Child []*Node
+}
+
 func main() {
 	i := []string(nil)
-	println("i", i == nil)
+	//println("i", i == nil)
 	x := make([]string, 0)
-	println("x", x == nil)
+	//println("x", x == nil)
 	result := append(i, x...)
 	expected := []string(nil)
+	if result != nil {
+		panic("result is not nil")
+	}
+	if expected != nil {
+		panic("expected is not nil")
+	}
+	b := reflect.DeepEqual(result, expected)
+	if !b {
+		panic("invalid deepEqual")
+	}
 
-	println("result", result == nil)
-	println("expected", expected == nil)
-	println(reflect.DeepEqual(result, expected))
+	n := &Node{Name: "parent"}
+	n.Child = append(n.Child, &Node{Name: "child"})
+	fmt.Println(len(n.Child))
+	if n.Name != "parent" {
+		panic("incorrect n.Name: " + n.Name)
+	}
+	if n.Child[0].Name != "child" {
+		panic("incorrect n.Child[0].Name: " + n.Child[0].Name)
+	}
 }

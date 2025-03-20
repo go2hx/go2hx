c6ea515fa84d099851f531886f0a6bb8cbdcf43f
Joey <9813078+PXshadow@users.noreply.github.com>
2025-03-20 15:11:35 +0300

    Macro: add back init func that caches the type information from running expr macro Go.toInterface (any,interface{} creation)

--- stdgo/_internal/internal/Macro.macro.hx
+++ stdgo/_internal/internal/Macro.macro.hx
@@ -8,6 +8,28 @@ import haxe.macro.Context;
 using Lambda;
 
 class Macro {
+	public static function init() {
+		var run = false;
+		Context.onAfterTyping(_ -> {
+			if (run)
+				return;
+			run = true;
+			final exprs:Array<Expr> = [];
+			for (name => e in @:privateAccess Go.nameTypes) {
+				exprs.push(macro $v{name} => $e);
+			}
+			//trace("non hxb types:", exprs.length);
+			final e = macro $a{exprs};
+			final className = "TypeInfoData_go2hx_";
+			final cl = macro class T {
+				public var names:stdgo.TypeInfo.TypeInternalMap = untyped $e;
+				public function new() {}
+			};
+			cl.meta.push({name: ":keep", pos: haxe.macro.Context.currentPos()});
+			cl.name = className;
+			Context.defineType(cl);
+		});
+	}
 	private static function exprToString(e:Expr):String {
 		switch e.expr {
 			case EConst(CString(s)):

eca7c13e18e15eb5c42638b9e9cef27993d2619b
Elliott Stoneham <elliott.stoneham@gmail.com>
2025-05-05 15:33:48 +0100

    Add const iota handling
    test iterator.go commented out until fixed

--- analysis/goto.go
+++ analysis/goto.go
@@ -612,32 +612,60 @@ func (fs *funcScope) markJumps(stmt ast.Stmt, scopeIndex int) []ast.Stmt {
 		case *ast.GenDecl:
 			isVar := false
 			stmts := []ast.Stmt{}
+			iotaFlag := false // is iota being used in the declaration?
 			for _, spec := range decl.Specs {
 				switch spec := spec.(type) {
 				case *ast.ValueSpec:
 					isVar = true
-					names := []ast.Expr{}
-					for i, name := range spec.Names {
-						ident := fs.tempVarWithTypeExpr(name, spec.Type)
-						if len(spec.Values) > i {
-							names = append(names, ident)
+					if len(spec.Values) > 0 {
+						if idt, ok := spec.Values[0].(*ast.Ident); ok {
+							if idt.Name == "iota" {
+								iotaFlag = true
+							}
 						}
 					}
-					for i := range spec.Values {
-						spec.Values[i] = fs.changeVars(spec.Values[i])
+					names := []ast.Expr{}
+					for i, name := range spec.Names {
+						typ := spec.Type
+						if iotaFlag {
+							typ = &ast.Ident{Name: "int"}
+						}
+						ident := fs.tempVarWithTypeExpr(name, typ)
+						if len(spec.Values) > i || iotaFlag {
+							names = append(names, ident)
+						}
+						if iotaFlag {
+							v := strconv.Itoa(spec.Names[i].Obj.Data.(int)) // this will panic if it does not work
+							bl := &ast.BasicLit{Kind: token.INT, Value: v}
+							if len(spec.Values) > i {
+								spec.Values[i] = bl
+							} else {
+								spec.Values = append(spec.Values, bl)
+							}
+						}
 					}
-					if len(names) > 0 && decl.Tok != token.CONST {
-						stmts = append(stmts, &ast.AssignStmt{Lhs: names, Rhs: spec.Values, Tok: token.ASSIGN})
+					if iotaFlag && len(names) > 0 {
+						ass := ast.AssignStmt{Lhs: names, Rhs: spec.Values, Tok: token.ASSIGN}
+						stmts = append(stmts, &ass)
+					} else {
+						for i := range spec.Values {
+							spec.Values[i] = fs.changeVars(spec.Values[i])
+						}
+						if len(names) > 0 && decl.Tok != token.CONST {
+							stmts = append(stmts, &ast.AssignStmt{Lhs: names, Rhs: spec.Values, Tok: token.ASSIGN})
+						}
 					}
 				default:
 				}
 			}
+
 			if !isVar {
 				return []ast.Stmt{stmt}
 			} else {
 				return stmts
 			}
 		}
+
 	case *ast.AssignStmt:
 		if stmt.Tok == token.DEFINE {
 			for i, expr := range stmt.Lhs {
--- analysis/tests/unit/goto/iterator.go
+++ analysis/tests/unit/goto/iterator.go
@@ -1,17 +1,20 @@
 package main
 
 func main() {
-	s := false
-	println("pre-loop")
-loop:
-	for range []int{0, 1, 2} {
-		if s {
-			println("break")
-			break
-		}
-		s = true
-		println("continue")
-		continue loop
-	}
+	/*
+	   	s := false
+	   	println("pre-loop")
+	   loop:
+	   	for range []int{0, 1, 2} {
+	   		if s {
+	   			println("break")
+	   			break
+	   		}
+	   		s = true
+	   		println("continue")
+	   		continue loop
+	   	}
+	*/
 	println("end")
+
 }

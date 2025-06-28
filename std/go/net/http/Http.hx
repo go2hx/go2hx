43c9ae74588b96c068f59c9526c0959577d3ff4c
Joey <9813078+PXshadow@users.noreply.github.com>
2025-06-29 00:03:41 +0300

    std: move transport recv methods to net/http

--- std/go/net/http/Http.hx
+++ std/go/net/http/Http.hx
@@ -2,3 +2,16 @@ package go.net.http;
 
 function _newTLSConfig()
     return null;
+
+@:recv(Transport)
+function _onceSetNextProtoDefaults() {}
+
+
+@:recv(Transport)
+function _replaceReqCanceler() {
+	return true;
+}
+
+
+@:recv(Transport)
+function registerProtocol() {}

54f0cc174edbba5352f315be1aedeff7fe1e5175
Joey <9813078+PXshadow@users.noreply.github.com>
2025-07-13 09:21:28 +0300

    typer: turn Interface into AnyInterface for now when asserting

--- src/typer/exprs/Assert.hx
+++ src/typer/exprs/Assert.hx
@@ -17,12 +17,11 @@ function typeAssertExpr(expr:GoAst.TypeAssertExpr, info:Info):MacroExpr { // a -
 	final t = typeof(expr.type, info, false);
 	if (isAnyInterface(fromType))
 		return setAssert(e, t, ct, info);
-	e = macro $e.__underlying__();
+	e = typer.exprs.Expr.toAnyInterface(e, fromType, info);
 	return setAssert(e, t, ct, info);
 }
 
 private function setAssert(e:haxe.macro.Expr, gt:GoType, ct:ComplexType, info:Info):Expr {
 	final rt = toReflectType(gt, info, [], false);
-	//final t = macro new stdgo._internal.internal.reflect.Reflect._Type($rt);
 	return macro (stdgo.Go.typeAssert($e, $rt) : $ct);
 }

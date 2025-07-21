c65ee4a0ebac67aba34f97b78f27e21c9ec2c725
Joey <9813078+PXshadow@users.noreply.github.com>
2025-07-21 11:15:45 +0300

    typer: handle case of syscall and other funcs that have no bodies with a return type

--- src/typer/stmts/Block.hx
+++ src/typer/stmts/Block.hx
@@ -1,7 +1,7 @@
 package typer.stmts;
 
 function typeBlockStmt(stmt:GoAst.BlockStmt, info:Info, isFunc:Bool):MacroExpr {
-	if (stmt.list == null && stmt.lbrace == -1) {
+	if (stmt.list == null && (stmt.lbrace == -1 || info.returnTypes.length > 0)) {
 		if (isFunc) {
 			final s = HaxeAst.makeString("not implemented: " + info.funcName);
 			return macro {

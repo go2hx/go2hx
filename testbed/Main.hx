7bb03b45db1a7dd8a3e1904ae8c859cf0c319ca7
Joey <9813078+PXshadow@users.noreply.github.com>
2025-07-28 09:34:59 +0300

    testbed: change Main to always compile

--- testbed/Main.hx
+++ testbed/Main.hx
@@ -1,26 +1,3 @@
-import go.net.http.Http;
 function main() {
-    // issue
-    // Http.handle("/", Http.fileServer(go.Go.asInterface(("." : go._internal.net.http.Http_dir.Dir))));
-    Http.handleFunc("/export.go", (struct, request) -> {
-        Http.serveFile(struct, request, "export.go");
-    });
-    Http.handleFunc("/go4hx.exe", (struct, request) -> {
-        Http.serveFile(struct, request, "go4hx.exe");
-    });
-
-    Http.handleFunc("/check.json", (struct, request) -> {
-        Http.serveFile(struct, request, "check.json");
-    });
-    Http.handleFunc("/query", (response, request) -> {
-        var count = 0;
-        var buf:String = "";
-        for(key => value in request.uRL.query()) {
-            trace(key, value);
-            buf += key + " : " + value.toString() + "\r\n";
-        }
-        response.write(buf);
-    });
-    trace("start!");
-    Http.listenAndServe("127.0.0.1:8090", null);
+    trace("Hello from Haxe!");
 }

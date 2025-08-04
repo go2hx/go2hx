ff35d1f5dbec17e2751c348e104691667c4d9f38
Joey <9813078+PXshadow@users.noreply.github.com>
2025-08-04 15:03:28 +0300

    stmts: Defer allow proper casting of args

--- src/typer/stmts/Defer.hx
+++ src/typer/stmts/Defer.hx
@@ -3,37 +3,19 @@ package typer.stmts;
 function typeDeferStmt(stmt:GoAst.DeferStmt, info:Info):MacroExpr {
 	info.global.deferBool = true;
 	var exprs:Array<Expr> = [];
-	final localIdents = [];
-	// copy over
-	final call:GoAst.CallExpr = {
-		fun: stmt.call.fun,
-		lparen: stmt.call.lparen,
-		args: stmt.call.args.copy(),
-		ellipsis: stmt.call.ellipsis,
-		rparen: stmt.call.rparen,
-		type: stmt.call.type,
-		objType: stmt.call.objType,
-		typeArgs: stmt.call.typeArgs,
-	};
-	for (i in 0...stmt.call.args.length) {
-		var arg = typer.exprs.Expr.typeExpr(call.args[i], info);
-		var name = "_a" + i;
-		localIdents.push(name);
-		final e = macro var $name = $arg;
-		exprs.push(e);
-		call.args[i] = {id: "Ident", name: 'a$i', type: call.args[i].type}; // switch out call arguments
-	}
-	info.localIdents = info.localIdents.concat(localIdents);
-	// otherwise its Ident, Selector etc
-	var call = typer.exprs.Call.typeCallExpr(call, info);
+	var call = typer.exprs.Call.typeCallExpr(stmt.call, info);
 	switch call.expr {
 		case ECall(e, params):
 			exprs.push(macro final __f__ = $e);
+			for (i in 0...params.length) {
+				var name = "_a" + i;
+				final e = macro var $name = ${params[i]};
+				exprs.push(e);
+				params[i] = macro $i{name};
+			}
 			call = macro __f__($a{params});
 		default:
 	}
-
-	info.localIdents = info.localIdents.slice(0, info.localIdents.length - localIdents.length);
 	var e = macro __deferstack__.unshift({ran: false, f: () -> $call});
 	return macro $b{exprs.concat([e])};
 }

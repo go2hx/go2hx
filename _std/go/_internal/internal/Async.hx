0bc81522e6f78f1e5f1c77607815a60c8671bae8
Joey <joeypxshadow@duck.com>
2025-11-01 04:13:57 -0700

    std: Async, tick should occur in js automatically

--- _std/go/_internal/internal/Async.hx
+++ _std/go/_internal/internal/Async.hx
@@ -3,10 +3,7 @@ package go._internal.internal;
 class Async {
 	public static function tick() {
 		#if (target.threaded && !cppia)
-		
 		sys.thread.Thread.current().events.loopOnce();
-		#elseif !js
-		@:privateAccess haxe.MainLoop.tick();
 		#end
 	}
 }

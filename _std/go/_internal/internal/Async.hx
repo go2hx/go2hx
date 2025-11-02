9fac29c7f4c050ccc9d0574f2f0c067116304d89
Joey <joeypxshadow@duck.com>
2025-11-02 12:27:16 +0100

    Disable goroutine define (#285)
    * std: Thread.createWithEventLoop -> create, events.progress() ->events.loopOnce()
    
    * .haxerc: update Haxe to version when refactor event loop is pushed into dev branch
    
    * std: Go routine not blocking at call point
    
    * std: Async, tick should occur in js automatically
    
    * actions: remove haxe preview 5 testing
    
    * README: mention Haxe 5 nightly version required
    
    * std: Go add Compiler define to disable goroutine -D disable_goroutine

--- _std/go/_internal/internal/Async.hx
+++ _std/go/_internal/internal/Async.hx
@@ -3,9 +3,7 @@ package go._internal.internal;
 class Async {
 	public static function tick() {
 		#if (target.threaded && !cppia)
-		sys.thread.Thread.current().events.progress();
-		#elseif !js
-		@:privateAccess haxe.MainLoop.tick();
+		sys.thread.Thread.current().events.loopOnce();
 		#end
 	}
 }
